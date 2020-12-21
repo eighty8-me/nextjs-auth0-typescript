07f22b88a4057840e7c3f42b50f19078ccb31cad
Tsunemasa Hachiya <tsunemasa.hachiya@label8.jp>
2020-12-21 19:07:05 +0900

    initial commit

---
+++ lib/auth0.ts
@@ -0,0 +1,25 @@
+import { initAuth0 } from '@auth0/nextjs-auth0';
+
+function getServerSetting(environmentVariable: string, defaultValue?: string) {
+  if (typeof window === 'undefined') {
+    return process.env[environmentVariable];
+  }
+
+  return defaultValue || null;
+}
+
+export default initAuth0({
+  clientId: getServerSetting('AUTH0_CLIENT_ID'),
+  clientSecret: getServerSetting('AUTH0_CLIENT_SECRET'),
+  scope: 'openid profile email',
+  domain: getServerSetting('AUTH0_DOMAIN'),
+  redirectUri: getServerSetting('AUTH0_REDIRECT_URI', 'http://localhost:3000/api/callback'),
+  postLogoutRedirectUri: getServerSetting('AUTH0_POST_LOGOUT_REDIRECT_URI', 'http://localhost:3000/'),
+  session: {
+    cookieSecret: getServerSetting('AUTH0_COOKIE_SECRET'),
+    cookieLifetime: 7200,
+    storeIdToken: true,
+    storeAccessToken: true,
+    storeRefreshToken: true,
+  }
+});
---
+++ pages/api/login.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import auth0 from '../../lib/auth0';
+
+export default async function login(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  try {
+    await auth0.handleLogin(req, res, {
+      authParams: {
+        audience: 'http://localhost:8009/',
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+}
